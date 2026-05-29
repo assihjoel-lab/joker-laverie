52bedbeaaaee9da7f9bd2aa497f920f84cc2828a
assihjoel-lab <assihjoel@gmail.com>
2026-05-29 21:38:54 +0000

    joker-v12

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "joker-v11";
+const CACHE = "joker-v12";
 const ASSETS = ["/", "/index.html", "/static/js/main.chunk.js", "/static/css/main.chunk.css"];
 
 self.addEventListener("install", e => {
